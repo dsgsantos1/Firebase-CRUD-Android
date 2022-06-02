4d3e4f292aeb0fcab98ce6e43cc015ace8441404
dsgsantos1 <dsgsantos1@gmail.com>
2022-06-02 17:04:37 -0400

    Adicionando o que falta

---
+++ app/src/main/java/com/danhenrico/firebasecrud/RecuperaActivity.java
@@ -0,0 +1,14 @@
+package com.danhenrico.firebasecrud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecuperaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recupera);
+    }
+}
