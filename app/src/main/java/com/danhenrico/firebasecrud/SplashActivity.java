3b9601c3965015ee734104799f63e47881b0b591
dsgsantos1 <dsgsantos1@gmail.com>
2022-06-02 17:03:33 -0400

    XML das telas de Login e Cadastro criadas

--- app/src/main/java/com/danhenrico/firebasecrud/SplashActivity.java
+++ app/src/main/java/com/danhenrico/firebasecrud/SplashActivity.java
@@ -2,7 +2,9 @@ package com.danhenrico.firebasecrud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //Manda pra outra tela depois de x milisegundos
+        new Handler(getMainLooper()).postDelayed(() -> {
+            finish();
+            startActivity(new Intent(this, LoginActivity.class));
+        }, 1000);
     }
 }
